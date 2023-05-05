aed509cc5f69523b300fe9032383e59873771703
Gabriel Costa <noobox-dev@hotmail.com>
2023-05-05 17:27:22 -0300

    Update conn.php

--- Connections/conn.php
+++ Connections/conn.php
@@ -8,7 +8,7 @@ $database_conn = "uploaddocs";
 $username_conn = "root";
 $password_conn = "";
 $conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
-$site_url = "http://localhost/cristianocontabilidade/";
-$sitename = "Cristiano Contabilidade";
+$site_url = "http://localhost/";
+$sitename = "UploadBox";
 $file_path = "files/";
 ?>
